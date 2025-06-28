932f2ea324ba4cc7b88aac2b473ee1ace2d0ab50
KwamK-Code <kouassi.kwam@gmail.com>
2025-06-28 19:22:02 +0100

    version 1.1

--- dags/modules/extracts_tasks.py
+++ dags/modules/extracts_tasks.py
@@ -1,44 +1,51 @@
 import sys
+
 sys.path.append("/opt/airflow/dags/modules/")
 from data_downloader import *
-import os
+
 import json
 
-dag_dir = os.path.dirname(os.path.abspath(__file__))
-config_path = os.path.join(dag_dir, 'config.json')
+config_path = "dags/modules/config.json"
 
 with open(config_path) as stream:
     config = json.load(stream)
 
+
 def prices_task():
 
-    price_extract = Prices(get_input(), starting_date=config['start_date'], ending_date=config['end_date'])
+    price_extract = Prices(
+        get_input(), starting_date=config["start_date"], ending_date=config["end_date"]
+    )
     price_extract.get_data()
+
 
 def earnings_estimate_task():
 
     earnings_estimate_extract = Earnings_Estimate(get_input())
     earnings_estimate_extract.get_data()
 
+
 def earnings_history_task():
 
     earnings_history_extract = Earnings_History(get_input())
     earnings_history_extract.get_data()
+
 
 def info_task():
 
     info_extract = Info(get_input())
     info_extract.get_data()
 
+
 def get_input():
-    file_ticker = config['path_inputs']
-    # file_ticker = 'inputs/sp500_list.csv' # uncomment for ide run
-    df_ticker = pd.read_csv(file_ticker,encoding='utf-8',sep=';')['Symbol']
+    file_ticker = config["path_inputs"]
+    df_ticker = pd.read_csv(file_ticker, encoding="utf-8", sep=";")["Symbol"]
     list_ticker = list(df_ticker)
 
     return list_ticker
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     prices_task()
     # earnings_estimate_task()
