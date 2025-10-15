6211c355bdf0e270ec15fe70676c6d2f6a53de49
unknown <lotfidoucanef@hotmail.fr>
2025-10-15 07:39:17 +0200

    add comments in init sql

--- db/init/01-init.sql
+++ db/init/01-init.sql
@@ -1,3 +1,8 @@
+-- Create a regular database role (login) named 'users' with its password.
 CREATE USER users WITH PASSWORD 'users';
+
+-- Create the 'users' database and set 'users' as its owner (not a superuser).
 CREATE DATABASE users OWNER users;
+
+-- Grant full privileges on the 'users' database to the 'users' role (connect/create).
 GRANT ALL PRIVILEGES ON DATABASE users TO users;
