b74883cde7bdfef131e5c0d371ae8538a361e6c9
unknown <lotfidoucanef@hotmail.fr>
2025-10-23 17:23:17 +0200

    CRUD is working

--- backend/src/users/users_model.py
+++ backend/src/users/users_model.py
@@ -1,11 +1,13 @@
 from ..infra.db import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
+import uuid
 
 class Users(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, default=uuid.uuid4)
     user_name = Column(String(120), nullable=False, unique=True, index=True)
     email = Column(String(255), nullable=False, unique=True, index=True)
-    hashed_password = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
+    is_admin = Column(Boolean, default=False)
 
